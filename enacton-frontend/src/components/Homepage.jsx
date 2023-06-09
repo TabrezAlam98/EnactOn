3304ceed0b415a14741b2ffc20f76bbaf64e43c9
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-06-09 14:14:56 +0530

    initial commit

---
+++ enacton-frontend/src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    //main div
+    <div className="w-screen h-[2302px] bg-[#F2F7FB]">
+      <div className="w-[312px] h-[1051px] bg-[#FFFFFF]">homepage</div>
+    </div>
+  );
+};
+
+export default Homepage;
